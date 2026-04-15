68e81e7faf2ba5b2d07f34b8d787c56d09705cd5
MrDolev <20320563+MrDolev@users.noreply.github.com>
2026-04-16 00:23:49 +0200

    test: :adhesive_bandage: add test about worker with async desing using atomic counter JTBD

---
+++ pkg/semaphore/worker_test.go
@@ -0,0 +1,63 @@
+package semaphore
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestWorkerJob(t *testing.T) {
+	tests := []struct {
+		name          string
+		semaphoreSize int
+		jobCount      int
+	}{
+		{
+			name:          "semaphore limits concurrent access",
+			semaphoreSize: 3,
+			jobCount:      10,
+		},
+		{
+			name:          "single semaphore permit",
+			semaphoreSize: 1,
+			jobCount:      5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var wg sync.WaitGroup = sync.WaitGroup{}
+			w := &Worker{
+				wg: &wg,
+				semaphore: Semaphore{
+					Channel: make(chan struct{}, tt.jobCount),
+				},
+			}
+			cj := NewCompletedJobs()
+			for i := 0; i < tt.jobCount; i++ {
+				w.wg.Add(1)
+				cj.jobDone(i, w)
+			}
+			w.wg.Wait()
+			if tt.jobCount != cj.Count() {
+				t.Errorf("Runner() = %v, want %v", cj.completed, tt.jobCount)
+			}
+		})
+	}
+}
+
+type CompletedJobs struct {
+	completed atomic.Int64
+}
+
+func NewCompletedJobs() *CompletedJobs {
+	return &CompletedJobs{}
+}
+
+func (cj *CompletedJobs) jobDone(id int, worker *Worker) {
+	go worker.Job(id)
+	cj.completed.Add(1)
+}
+
+func (cj *CompletedJobs) Count() int {
+	return int(cj.completed.Load())
+}
